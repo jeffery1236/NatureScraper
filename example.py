a3e80d66da576144116b52551fa51be21fb802bc
Jeffery <jeffery1236@gmail.com>
2021-08-05 16:52:06 +0800

    added coments on article schema

--- example.py
+++ example.py
@@ -3,5 +3,14 @@ from NatureScraper import Scraper
 scraper = Scraper()
 articles = scraper.extract_nature_articles('2020-02-07', '2020-02-12', 'coronavirus', save_pdf=True, pdf_dir='./saved_articles')
 
-titles = [a['Title'] for a in articles]
-print(titles)
+# articles is a list of dictionaries with the following fields
+# {
+#     'Title': str,
+#     'Author': List[str],
+#     'Published Date': str,
+#     'Url': str,
+#     'Summary': str,
+#     'Content': List[str]
+# }
+
+print(articles)
